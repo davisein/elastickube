ca8098e23c9821fcd9e9015d688b8ee7b04fc1f0
Alberto Arias Maestro <alberto@elasticbox.com>
2016-02-05 19:19:10 -0800

    Update authentication

--- src/api/v1/instances.py
+++ src/api/v1/instances.py
@@ -1,11 +1,71 @@
-from v1.secure import SecureHandler
+import logging
+import pymongo
+
+from motor.motor_tornado import MotorClient
+
+from tornado.gen import coroutine, sleep
+from tornado.web import HTTPError
+from tornado.websocket import WebSocketHandler
+from v1.secure import AuthenticationHandler
 
 
-class InstancesHandler(SecureHandler):
+class InstancesHandler(WebSocketHandler, AuthenticationHandler):
 
-    def get(self):
-        response = dict(
-            instances=[]
-        )
+    def __init__(self, application, request, **kwargs):
+        super(InstancesHandler, self).__init__(application, request, **kwargs)
 
-        self.write(response)
+        self.cursor = None
+        self.connected = False
+
+
+    @coroutine
+    def open(self,_):
+        logging.info("Initializing instances handler.")
+
+        try:
+            yield self.authenticate()
+
+            oplog = MotorClient("mongodb://elastickube-mongo:27017").local["oplog.rs"]
+            self.cursor = oplog.find().limit(-1)
+            self.connected = True
+            last_timestamp = None
+
+            while self.connected:
+                if (yield self.cursor.fetch_next):
+                        doc = self.cursor.next_object()
+                        self.write_message(str(doc))
+                        last_timestamp = doc["ts"]
+
+                if self.cursor and not self.cursor.alive:
+                    yield sleep(1)
+
+                    logging.debug("Recreating cursor.")
+                    self.cursor = oplog.find({ 'ts': {'$gt': last_timestamp} }, tailable=True, await_data=True)
+                    self.cursor.add_option(8)
+
+        except HTTPError:
+            self.write_message("Authentication failed.")
+
+        except Exception as e:
+            logging.exception(e)
+            self.write_message("Failed to connect to event source.")
+
+        finally:
+            self.close()
+
+    def on_message(self, message):
+        if "close" in message:
+            self.close()
+
+    @coroutine
+    def on_close(self):
+        logging.info("Closing InstancesHandler")
+
+        try:
+
+            if self.cursor and self.cursor.alive:
+                yield self.cursor.close()
+
+        finally:
+            self.cursor = None
+            self.connected = False
--- src/api/v1/secure.py
+++ src/api/v1/secure.py
@@ -1,23 +1,22 @@
 import jwt
+import logging
 
 from tornado.gen import coroutine
 from tornado.web import RequestHandler, HTTPError
 
+
 ELASTICKUBE_TOKEN_HEADER = "ElasticKube-Token"
 
-class SecureHandler(RequestHandler):
+class AuthenticationHandler():
 
     def __init__(self, application, request, **kwargs):
-        super(SecureHandler, self).__init__(application, request, **kwargs)
-
         self.user = None
 
     @coroutine
-    def prepare(self):
+    def authenticate(self):
         try:
             # Try the header if not the cookie
             encoded_token = self.request.headers.get(ELASTICKUBE_TOKEN_HEADER)
-
             if encoded_token is None:
                 encoded_token = self.get_cookie(ELASTICKUBE_TOKEN_HEADER)
 
@@ -28,10 +27,19 @@ class SecureHandler(RequestHandler):
             self.user = yield self.settings["database"].Users.find_one({"username": token["username"]})
 
             if self.user is None:
+                logging.debug("User not found.")
                 raise HTTPError(401, "Invalid token.")
 
         except jwt.DecodeError as e:
+            logging.debug("The token could not decoded.")
             raise HTTPError(401, "Invalid token.")
+
+
+class SecureHandler(RequestHandler, AuthenticationHandler):
+
+    @coroutine
+    def prepare(self):
+        yield self.authenticate()
 
 
 
