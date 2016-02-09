36472fefff5df653034b626dafdfb7effa0b6876
arnaud bonnet <arnaud@elasticbox.com>
2016-02-09 11:52:27 -0800

    Add SecureRequestHandler and SecureWebSocketHandler as base class for every handlers

--- src/api/v1/instances.py
+++ src/api/v1/instances.py
@@ -2,34 +2,32 @@ import logging
 
 from tornado.escape import json_encode
 from tornado.gen import coroutine, sleep
-from tornado.web import HTTPError
-from tornado.websocket import WebSocketHandler
 
 from api.kube_client import client
-from api.v1.secure import AuthenticationHandler
+from api.v1.secure import SecureWebSocketHandler
 
 
-class InstancesHandler(WebSocketHandler, AuthenticationHandler):
+class InstancesHandler(SecureWebSocketHandler):
 
     def __init__(self, application, request, **kwargs):
         super(InstancesHandler, self).__init__(application, request, **kwargs)
 
-        self.cursor = None
         self.namespace = None
         self.connected = False
         self.controllers = []
 
     @coroutine
-    def open(self, *args, **kwargs):
-        logging.info("Initializing instances handler for workspace {0}".format(args[0]))
+    def open(self, namespace):
+        logging.info("Initializing InstancesHandler for namespace '{0}'".format(namespace))
 
         try:
-            yield self.authenticate()
+            yield super(InstancesHandler, self).open()
+
+            self.namespace = namespace
 
             kube = client.Client('10.5.10.6:8080')
-            self.namespace = args[0]
-            self.connected = True
 
+            self.connected = True
             while self.connected:
                 response = yield kube.replication_controllers.get(namespace=self.namespace)
 
@@ -39,16 +37,15 @@ class InstancesHandler(WebSocketHandler, AuthenticationHandler):
                         new_controllers.append(controller)
                         self.controllers.append(controller['metadata']['uid'])
 
-                self.write_message(json_encode(new_controllers))
+                if len(new_controllers) > 0 and self.connected:
+                    self.write_message(json_encode(new_controllers))
 
                 yield sleep(1)
 
-        except HTTPError:
-            self.write_message("Authentication failed.")
-
         except Exception as e:
             logging.exception(e)
-            self.write_message("Failed to connect to event source.")
+            if self.connected:
+                self.write_message("Failed to connect to event source.")
 
         finally:
             self.close()
@@ -59,11 +56,7 @@ class InstancesHandler(WebSocketHandler, AuthenticationHandler):
 
     @coroutine
     def on_close(self):
-        logging.info("Closing InstancesHandler")
+        logging.info("Closing InstancesHandler for namespace '{0}'".format(self.namespace))
 
-        try:
-            if self.cursor and self.cursor.alive:
-                yield self.cursor.close()
-        finally:
-            self.cursor = None
-            self.connected = False
+        self.connected = False
+        yield super(InstancesHandler, self).on_close()
